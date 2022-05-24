a75cfa3d0269ad098302c18864b5389e3a30839c
CeViMu <vce02060@innovaccion.mx>
2022-05-23 23:01:28 -0500

    Agregando prueba fallida para segundo requerimeinto en el Controller

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -1,6 +1,13 @@
 const StudentController = require("./../../lib/controller/StudentController");
 
-test("Prueba para primer requerimiento", ()=>{
+test("Prueba para primer requerimiento, mostrar a los estudiantes", ()=>{
     const getStudents = StudentController.getStudents();
     expect(getStudents).not.toBeUndefined();
+})
+
+test("Prueba para segundo requerimiento, mostrar los correos de los estudiantes con certificación", ()=>{
+    const students = [{name:"Warren", email: "Todd@visualpartnership.xyz", haveCertification: true},
+                      {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
+    const StudentsWithCertification = StudentController.StudentsFilterByEmail(students);
+    expect(StudentsWithCertification.length).toBe(1);
 })
