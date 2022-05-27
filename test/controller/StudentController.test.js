2737eec534aaed69cc7cdecc240fd9051eba3309
CeViMu <vce02060@innovaccion.mx>
2022-05-26 21:24:54 -0500

    Modificando pruebas de requerimientos 2 y 3

--- test/controller/StudentController.test.js
+++ test/controller/StudentController.test.js
@@ -6,15 +6,13 @@ test("Prueba para primer requerimiento, mostrar a los estudiantes", ()=>{
 })
 
 test("Prueba para segundo requerimiento, mostrar los correos de los estudiantes con certificación", ()=>{
-    const students = [{name:"Warren", email: "Todd@visualpartnership.xyz", haveCertification: true},
-                      {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false}];
-    const StudentsWithCertification = StudentController.StudentsFilterByEmail(students);
-    expect(StudentsWithCertification.length).toBe(1);
+    const StudentsWithCertification = StudentController.StudentsFilterByEmail();
+    expect(StudentsWithCertification.length).toBe(29);
 })
 
 test("Prueba para tercer requerimiento, mostrar a los estudiantes con más de 500 créditos", ()=>{
     const students = [{name:"Warren", email: "Todd@visualpartnership.xyz", haveCertification: true, "credits": 499},
                      {name: "Phillips", email: "Camacho@visualpartnership.xyz", haveCertification: false, "credits": 973}];
     const StudentsWithMoreThan500Credits = StudentController.StudentsFilterByCredits(students);
-    expect(StudentsWithMoreThan500Credits.length).toBe(1);
+    expect(StudentsWithMoreThan500Credits.length).toBe(27);
 })
